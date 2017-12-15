bac8ec1b0ad936868bda37699c7373858c582440
davidcaball <davidcaballeroe@gmail.com>
2017-12-15 15:27:53 -0600

    updated bash compile script

--- compile.sh
+++ compile.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/bash
 
-g++ -c main.cpp menu.cpp game.cpp hero.cpp entity.cpp snake.cpp platform.cpp fireball.cpp controls.cpp
+
+g++ -c source/main.cpp source/menu.cpp source/game.cpp source/hero.cpp source/entity.cpp source/snake.cpp source/platform.cpp source/fireball.cpp source/controls.cpp
 g++ main.o menu.o game.o hero.o entity.o snake.o platform.o fireball.o controls.o -o main-app -lsfml-graphics -lsfml-window -lsfml-system -lsfml-audio
